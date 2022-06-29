49d10a2dae48cf077508c0a2999a905e383c805f
JoeriKaiser <joeri.kaiser@hotmail.com>
2022-06-29 16:40:46 +0200

    Removed base components, created Header and inserted assests

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 import "./App.css";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/pages/Projects.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/Header";
+
+function Projects() {
+  return (
+    <div>
+      <Header title="Projects" />
+    </div>
+  );
+}
+
+export default Projects;
