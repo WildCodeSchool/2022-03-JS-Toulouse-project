65b20ce68f42368414a5c1754a6e3a1063b6b1b9
JoeriKaiser <joeri.kaiser@hotmail.com>
2022-06-30 16:42:49 +0200

    Fixed card links

--- frontend/src/components/ProjectCard.jsx
+++ frontend/src/components/ProjectCard.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 import "../styles/ProjectCard.css";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { Link } from "react-router-dom";
 
-function ProjectCard({ title, description, image, technologies }) {
+function ProjectCard({ title, description, image, technologies, link }) {
   return (
-    <div className="project-container">
-      <img src={image} alt="placeholder" />
-      <div className="project-text-container">
-        <h2>{title}</h2>
-        <p>{description}</p>
+    <Link className="project-card-link" to={link}>
+      <div className="project-container">
+        <img src={image} alt="placeholder" />
+        <div className="project-text-container">
+          <h2>{title}</h2>
+          <p>{description}</p>
+        </div>
+        <ul className="tech-list">
+          {technologies.map((el) => (
+            <li className="tech-pill">{el.name}</li>
+          ))}
+        </ul>
+        <div className="arrow-container">
+          <KeyboardArrowDownIcon sx={{ margin: "0 auto" }} />
+        </div>
       </div>
-      <ul className="tech-list">
-        {technologies.map((el) => (
-          <li className="tech-pill">{el.name}</li>
-        ))}
-      </ul>
-      <div className="arrow-container">
-        <KeyboardArrowDownIcon sx={{ margin: "0 auto" }} />
-      </div>
-    </div>
+    </Link>
   );
 }
 
