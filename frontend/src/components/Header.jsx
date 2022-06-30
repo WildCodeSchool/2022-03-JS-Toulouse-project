58b20dc27f0c904e1ec414aefbdee5ef883d6e27
JoeriKaiser <joeri.kaiser@hotmail.com>
2022-06-30 16:24:26 +0200

    Finished project cards

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,60 +1,13 @@
-import { SpeedDial, SpeedDialAction } from "@mui/material";
 import React from "react";
 import "../styles/Header.css";
-import MenuIcon from "@mui/icons-material/Menu";
-import CottageIcon from "@mui/icons-material/Cottage";
-import GroupIcon from "@mui/icons-material/Group";
-import LightbulbIcon from "@mui/icons-material/Lightbulb";
-import WorkIcon from "@mui/icons-material/Work";
-import NewspaperIcon from "@mui/icons-material/Newspaper";
-import { Person } from "@mui/icons-material";
-import { useNavigate } from "react-router-dom";
 import HeaderLogo from "./HeaderLogo";
 
-function Header() {
-  const navigate = useNavigate();
-  const actions = [
-    { icon: <CottageIcon />, name: "DASHBOARD", navigate: "/" },
-    { icon: <GroupIcon />, name: "COLLABORATORS", navigate: "/" },
-    { icon: <LightbulbIcon />, name: "IDEAS", navigate: "/" },
-    { icon: <WorkIcon />, name: "CUSTOMERS", navigate: "/" },
-    { icon: <NewspaperIcon />, name: "PRESS REVIEW", navigate: "/" },
-    { icon: <Person />, name: "ACCOUNT", navigate: "/" },
-  ];
+function Header({ name }) {
   return (
     <div className="header-container">
       <HeaderLogo />
-      <h2 className="header-title">projects</h2>
-      <div>
-        <SpeedDial
-          ariaLabel="SpeedDial"
-          sx={{
-            position: "absolute",
-            top: 16,
-            right: 16,
-          }}
-          direction="down"
-          icon={<MenuIcon />}
-          FabProps={{
-            sx: {
-              bgcolor: "#e79759",
-              "&:hover": {
-                bgcolor: "#9C5D2D",
-              },
-            },
-          }}
-        >
-          {actions.map((action) => (
-            <SpeedDialAction
-              tooltipTitle={action.name}
-              tooltipOpen
-              key={action.name}
-              icon={action.icon}
-              onClick={() => navigate(action.navigate)}
-            />
-          ))}
-        </SpeedDial>
-      </div>
+      <h2 className="header-title">{name}</h2>
+      <div />
     </div>
   );
 }
--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "../styles/Projects.css";
+import SpeedDialComp from "../components/SpeedDial";
 import ProjectCard from "../components/ProjectCard";
 import Sidebar from "../components/Sidebar";
 import Header from "../components/Header";
 import projet1 from "../assets/projet1.png";
 import projet2 from "../assets/projet2.jpg";
 import projet3 from "../assets/projet3.jpg";
+import projet4 from "../assets/projet4.jpg";
 
 const projects = [
   {
@@ -68,12 +70,33 @@ const projects = [
     ],
     status: "finished",
   },
+  {
+    title: "Sed ut perspiciatis unde omnis",
+    description:
+      "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.",
+    image: projet4,
+    technologies: [
+      {
+        name: "#react",
+      },
+      {
+        name: "#Node",
+      },
+      {
+        name: "#GraphQL",
+      },
+      {
+        name: "...more",
+      },
+    ],
+    status: "finished",
+  },
 ];
 
 function Projects() {
   return (
     <div>
-      <Header title="Projects" />
+      <Header name="PROJECTS" />
       <section className="main-projects-container">
         <Sidebar />
         <div className="projects-center-container">
@@ -84,6 +107,7 @@ function Projects() {
         </div>
         <div>Projects</div>
       </section>
+      <SpeedDialComp />
     </div>
   );
 }
