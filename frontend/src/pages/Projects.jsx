f72eaf068847514e69f7afd0796e24b2cd221679
JoeriKaiser <joeri.kaiser@hotmail.com>
2022-06-30 16:01:42 +0200

    Halfway cards

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,7 +1,74 @@
 import React from "react";
 import "../styles/Projects.css";
+import ProjectCard from "../components/ProjectCard";
 import Sidebar from "../components/Sidebar";
 import Header from "../components/Header";
+import projet1 from "../assets/projet1.png";
+import projet2 from "../assets/projet2.jpg";
+import projet3 from "../assets/projet3.jpg";
+
+const projects = [
+  {
+    title: "Sed ut perspiciatis unde omnis",
+    description:
+      "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.",
+    image: projet1,
+    technologies: [
+      {
+        name: "#react",
+      },
+      {
+        name: "#Node",
+      },
+      {
+        name: "#GraphQL",
+      },
+    ],
+    status: "finished",
+  },
+  {
+    title: "Sed ut perspiciatis unde omnis",
+    description:
+      "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.",
+    image: projet2,
+    technologies: [
+      {
+        name: "#react",
+      },
+      {
+        name: "#Node",
+      },
+      {
+        name: "#GraphQL",
+      },
+      {
+        name: "...more",
+      },
+    ],
+    status: "finished",
+  },
+  {
+    title: "Sed ut perspiciatis unde omnis",
+    description:
+      "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.",
+    image: projet3,
+    technologies: [
+      {
+        name: "#react",
+      },
+      {
+        name: "#Node",
+      },
+      {
+        name: "#GraphQL",
+      },
+      {
+        name: "...more",
+      },
+    ],
+    status: "finished",
+  },
+];
 
 function Projects() {
   return (
@@ -9,6 +76,12 @@ function Projects() {
       <Header title="Projects" />
       <section className="main-projects-container">
         <Sidebar />
+        <div className="projects-center-container">
+          {projects.map((el) => (
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            <ProjectCard {...el} />
+          ))}
+        </div>
         <div>Projects</div>
       </section>
     </div>
